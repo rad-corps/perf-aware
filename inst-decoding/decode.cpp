db1dbc327a4a70a8eb6c1da709190d59cb55386d
Adam Hulbert <hulbert.adam@gmail.com>
2025-07-02 17:14:25 +1000

    progress

--- inst-decoding/decode.cpp
+++ inst-decoding/decode.cpp
@@ -1,12 +1,36 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <bitset>
 
 int main()
 {
+    using u8 = unsigned char;
+
+    u8 MV_RM_2_R = 0b10001000;
+    u8 MV_IM_2_RM = 0b11000110;
+
     std::string line;
     std::ifstream file("./input/input01", std::ios::in | std::ios::binary);
+    std::fstream out("./input/out01.asm", std::ios::out);
 
-    unsigned char c = file.get();
-    unsigned char d = file.get();
+    out << "bits 16\r\n\r\n";
+
+    while (u8 byte1 = file.get())
+    {
+        if (byte1 == -1)
+        {
+            break;
+        }
+
+        u8 OPCODE_MASK = byte1 & 0b11111100;
+        bool DBIT_MASK = byte1 & 0b00000010;
+        bool WBIT_MASK = byte1 & 0b00000001;
+
+        if ((OPCODE_MASK & byte1) == MV_RM_2_R)
+        {
+            out << "mov ";
+        }
+        byte1 = file.get();
+    }
 }
