8489ac6f34e8b10e3999430dfc43eddef5619dd6
Adam Hulbert <hulbert.adam@gmail.com>
2025-07-03 09:50:24 +1000

    exercise one working

--- inst-decoding/decode.cpp
+++ inst-decoding/decode.cpp
@@ -3,16 +3,75 @@
 #include <iostream>
 #include <bitset>
 
+using u8 = unsigned char;
+
+// reg table
+const u8 REG_AX = 0b000; // 0x00
+const u8 REG_CX = 0b001; // 0x01
+const u8 REG_DX = 0b010; // 0x02
+const u8 REG_BX = 0b011; // 0x04
+const u8 REG_SP = 0b100; // 0x08
+const u8 REG_BP = 0b101; // 0x10
+const u8 REG_SI = 0b110; // 0x11
+const u8 REG_DI = 0b111; // 0x12
+
+std::string getRegisterOperandDecoded(u8 _3bitRegOperand, bool wbit)
+{
+    if (!wbit)
+    {
+        switch (_3bitRegOperand)
+        {
+        case REG_AX:
+            return "al";
+        case REG_CX:
+            return "cl";
+        case REG_DX:
+            return "dl";
+        case REG_BX:
+            return "bl";
+        case REG_SP:
+            return "ah";
+        case REG_BP:
+            return "ch";
+        case REG_SI:
+            return "dh";
+        case REG_DI:
+            return "bh";
+        }
+    }
+    else
+    {
+        switch (_3bitRegOperand)
+        {
+        case REG_AX:
+            return "ax";
+        case REG_CX:
+            return "cx";
+        case REG_DX:
+            return "dx";
+        case REG_BX:
+            return "bx";
+        case REG_SP:
+            return "sp";
+        case REG_BP:
+            return "bp";
+        case REG_SI:
+            return "si";
+        case REG_DI:
+            return "di";
+        }
+    }
+}
+
 int main()
 {
-    using u8 = unsigned char;
 
     u8 MV_RM_2_R = 0b10001000;
     u8 MV_IM_2_RM = 0b11000110;
 
     std::string line;
-    std::ifstream file("./input/input01", std::ios::in | std::ios::binary);
-    std::fstream out("./input/out01.asm", std::ios::out);
+    std::ifstream file("./input/input02", std::ios::in | std::ios::binary);
+    std::fstream out("./output/input02_disassembled.asm", std::ios::out);
 
     out << "bits 16\r\n\r\n";
 
@@ -28,39 +87,48 @@ int main()
 
     const u8 MOD_REGISTER_MODE = 0b11000000;
 
-    // reg table w=0
-    const u8 REG_AL = 0;
-    const u8 REG_CL = 0b0010000;
-    const u8 REG_DL = 0b0100000;
-    const u8 REG_BL = 0b0110000;
-    const u8 REG_AH = 0b1000000;
-    const u8 REG_CH = 0b1010000;
-    const u8 REG_DH = 0b1100000;
-    const u8 REG_BH = 0b1110000;
+    int safetyCounter = 0;
 
-    // reg table w=1
-    const u8 REG_AX = REG_AL;
-    const u8 REG_CX = REG_CL;
-    const u8 REG_DX = REG_DL;
-    const u8 REG_BX = REG_BL;
-    const u8 REG_SP = REG_AH;
-    const u8 REG_BP = REG_CH;
-    const u8 REG_SI = REG_DH;
-    const u8 REG_DI = REG_BH;
-
-    while (u8 byte1 = file.get())
+    while (true)
     {
-        if (byte1 == -1)
+        ++safetyCounter;
+        if (safetyCounter > 100)
         {
             break;
         }
 
+        u8 byte1 = file.get();
+        if (byte1 == (u8)-1)
+        {
+            break;
+        }
+
+        u8 byte2 = file.get();
+        if (byte2 == (u8)-1)
+        {
+            break;
+        }
+
+        // process byte1
+
         if ((OPCODE_MASK & byte1) == MV_RM_2_R)
         {
             out << "mov ";
+        }
+        const bool wbit = byte1 & WBIT_MASK;
+        const bool dbit = byte1 & DBIT_MASK;
 
-            // get the 2nd byte
-            u8 byte2 = file.get();
+        // process byte2
+
+        const u8 modVal = byte2 & MOD_MASK;
+        const u8 regVal = (byte2 & REG_MASK) >> 3;
+        const u8 rmVal = byte2 & RM_MASK;
+
+        // only supporting register mode
+        if (modVal == MOD_REGISTER_MODE)
+        {
+            out << getRegisterOperandDecoded(rmVal, wbit) << ", ";
+            out << getRegisterOperandDecoded(regVal, wbit) << "\r\n";
         }
     }
 }
